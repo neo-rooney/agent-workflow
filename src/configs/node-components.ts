3743062c30d614784b98327b22ae34b95de3f572
cheolhun.bae <cheolhun.bae@bankwareglobal.com>
2025-12-16 13:36:58 +0900

    feat: HTTP Requset Node 추가

--- src/configs/node-components.ts
+++ src/configs/node-components.ts
@@ -2,10 +2,12 @@ import type { NodeTypes } from "@xyflow/react";
 import { InitialNode } from "@/components/initial-node";
 import { NodeType } from "@/generated/prisma/enums";
 import { ManualTriggerNode } from "@/features/trigger/components/menual-trigger/node";
+import { HttpRequestNode } from "@/features/execution/components/http-request/node";
 
 export const nodeComponents = {
   [NodeType.INITIAL]: InitialNode,
   [NodeType.MAMUAL_TRIGGER]: ManualTriggerNode,
+  [NodeType.HTTP_REQUEST]: HttpRequestNode,
 } as const satisfies NodeTypes;
 
 export type RegisteredNodeType = keyof typeof nodeComponents;
---
+++ src/features/execution/components/http-request/node.tsx
@@ -0,0 +1,79 @@
+"use client";
+import { type Node, type NodeProps, useReactFlow } from "@xyflow/react";
+import { GlobeIcon } from "lucide-react";
+import { memo, useState } from "react";
+import { BaseExecutionNode } from "../base-execution-node";
+import { HttpRequestDialog } from "./dialog";
+
+type HttpRequestNodeData = {
+  endpoint?: string;
+  method?: "GET" | "POST" | "PUT" | "DELETE" | "PATCH";
+  body?: string;
+  [key: string]: unknown;
+};
+
+type HttpRequestNodeType = Node<HttpRequestNodeData>;
+
+export const HttpRequestNode = memo((props: NodeProps<HttpRequestNodeType>) => {
+  const nodeData = props.data;
+  const { setNodes } = useReactFlow();
+  const description = nodeData?.endpoint
+    ? `${nodeData.method || "GET"}: ${nodeData.endpoint}`
+    : "Not configured";
+
+  const nodeStatus = "initial";
+
+  const [dialogOpen, setDialogOpen] = useState(false);
+
+  const handleOpenSettings = () => {
+    setDialogOpen(true);
+  };
+
+  const handleSubmit = (values: {
+    endpoint: string;
+    method: "GET" | "POST" | "PUT" | "DELETE" | "PATCH";
+    body?: string;
+  }) => {
+    setNodes((nodes) =>
+      nodes.map((node) => {
+        if (node.id === props.id) {
+          return {
+            ...node,
+            data: {
+              ...node.data,
+              endpoint: values.endpoint,
+              method: values.method,
+              body: values.body,
+            },
+          };
+        }
+        return node;
+      }),
+    );
+  };
+
+  return (
+    <>
+      <BaseExecutionNode
+        {...props}
+        id={props.id}
+        icon={GlobeIcon}
+        name="HTTP Request"
+        description={description}
+        onSettings={handleOpenSettings}
+        onDoubleClick={handleOpenSettings}
+        status={nodeStatus}
+      />
+      <HttpRequestDialog
+        open={dialogOpen}
+        onOpenChange={setDialogOpen}
+        onSubmit={handleSubmit}
+        defaultEndpoint={nodeData.endpoint}
+        defaultMethod={nodeData.method}
+        defaultBody={nodeData.body}
+      />
+    </>
+  );
+});
+
+HttpRequestNode.displayName = "HttpRequestNode";
