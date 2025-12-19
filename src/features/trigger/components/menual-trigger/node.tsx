e9a02a6a06ee136f5e610451d4565196aeb036e2
cheolhun.bae <cheolhun.bae@bankwareglobal.com>
2025-12-19 17:03:56 +0900

    bugfix: Menual Trigger Node nodeStatus 디버깅 코드 원복

--- src/features/trigger/components/menual-trigger/node.tsx
+++ src/features/trigger/components/menual-trigger/node.tsx
@@ -10,13 +10,12 @@ import { useNodeStatus } from "@/hooks/use-node-status";
 export const ManualTriggerNode = memo((props: NodeProps) => {
   const [dialogOpen, setDialogOpen] = useState(false);
 
-  // const nodeStatus = useNodeStatus({
-  //   nodeId: props.id,
-  //   channel: MANUAL_TRIGGER_CHANNEL_NAME,
-  //   topic: "status",
-  //   refreshToken: fetchManualTriggerRealtimeToken,
-  // });
-  const nodeStatus = "error";
+  const nodeStatus = useNodeStatus({
+    nodeId: props.id,
+    channel: MANUAL_TRIGGER_CHANNEL_NAME,
+    topic: "status",
+    refreshToken: fetchManualTriggerRealtimeToken,
+  });
 
   const handleOpenSettings = () => {
     setDialogOpen(true);
