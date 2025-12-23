b4da56a13def99bba59a43b8668615a6ca18738e
cheolhun.bae <cheolhun.bae@bankwareglobal.com>
2025-12-23 16:05:54 +0900

    refector: Execution history hook 이름 변경

--- src/features/execution-history/hooks/use-execution-history.ts
+++ src/features/execution-history/hooks/use-execution-history.ts
@@ -5,7 +5,7 @@ import { useExecutionHistoryParams } from "./use-execution-history-params";
  * 모든 실행 기록을 가져오는 훅, suspense 적용
  * @returns
  */
-export const useSuspenseExecutions = () => {
+export const useSuspenseExecutionHistories = () => {
   const trpc = useTRPC();
   const [params] = useExecutionHistoryParams();
   return useSuspenseQuery(trpc.executionHistory.getMany.queryOptions(params));
@@ -14,7 +14,7 @@ export const useSuspenseExecutions = () => {
 /**
  * 하나의 실행 기록을 가져오는 훅, suspense 적용
  */
-export const useSuspenseExecution = (id: string) => {
+export const useSuspenseExecutionHistory = (id: string) => {
   const trpc = useTRPC();
   return useSuspenseQuery(trpc.executionHistory.getOne.queryOptions({ id }));
 };
