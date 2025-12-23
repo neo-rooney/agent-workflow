2ae3c40339a8ce798e3bd057e7e0de1960e53925
cheolhun.bae <cheolhun.bae@bankwareglobal.com>
2025-12-23 15:47:08 +0900

    feat: Execution history router 추가

---
+++ src/features/execution-history/server/router.ts
@@ -0,0 +1,85 @@
+import { z } from "zod";
+import { PAGINATION } from "@/configs/constants";
+import prisma from "@/lib/prisma";
+import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
+
+export const executionHistoryRouter = createTRPCRouter({
+  getOne: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ ctx, input }) => {
+      return prisma.executionHistory.findUniqueOrThrow({
+        where: {
+          id: input.id,
+          workflow: {
+            userId: ctx.auth.user.id,
+          },
+        },
+        include: {
+          workflow: {
+            select: {
+              name: true,
+              id: true,
+            },
+          },
+        },
+      });
+    }),
+  getMany: protectedProcedure
+    .input(
+      z.object({
+        page: z.number().default(PAGINATION.DEFAULT_PAGE),
+        pageSize: z
+          .number()
+          .min(PAGINATION.MIN_PAGE_SIZE)
+          .max(PAGINATION.MAX_PAGE_SIZE)
+          .default(PAGINATION.DEFAULT_PAGE_SIZE),
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      const { page, pageSize } = input;
+
+      const [items, totalCount] = await Promise.all([
+        prisma.executionHistory.findMany({
+          skip: (page - 1) * pageSize,
+          take: pageSize,
+          where: {
+            workflow: {
+              userId: ctx.auth.user.id,
+            },
+          },
+          orderBy: {
+            startedAt: "desc",
+          },
+          include: {
+            workflow: {
+              select: {
+                name: true,
+                id: true,
+              },
+            },
+          },
+        }),
+        prisma.executionHistory.count({
+          where: {
+            workflow: {
+              userId: ctx.auth.user.id,
+            },
+          },
+        }),
+      ]);
+
+      const totalPages = Math.ceil(totalCount / pageSize);
+      const hasNextPage = page < totalPages;
+      const hasPreviousPage = page > 1;
+
+      return {
+        items,
+        page,
+        pageSize,
+        hasNextPage,
+        hasPreviousPage,
+        totalCount,
+        totalPages,
+      };
+    }),
+});
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,10 +1,12 @@
 import { createTRPCRouter } from "../init";
 import { workflowRouter } from "@/features/workflow/server/router";
 import { credentialsRouter } from "@/features/credential/server/router";
+import { executionHistoryRouter } from "@/features/execution-history/server/router";
 
 export const appRouter = createTRPCRouter({
   workflow: workflowRouter,
   credential: credentialsRouter,
+  executionHistory: executionHistoryRouter,
 });
 
 export type AppRouter = typeof appRouter;
